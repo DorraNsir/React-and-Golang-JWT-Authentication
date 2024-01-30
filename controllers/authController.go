4aface57e546c1d36881da29e43ce9d4a691b469
Dorra Nsir <dorransir24@gmail.com>
2024-01-30 14:37:53 +0100

    Update login function

--- controllers/authController.go
+++ controllers/authController.go
@@ -3,15 +3,17 @@ package controllers
 import (
 	"auth-go/database"
 	"auth-go/models"
-
+	"strconv"
+	"time"
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/crypto/bcrypt"
+	"github.com/dgrijalva/jwt-go"
 )
-
+const SecretKey="secret"
 func Register(c *fiber.Ctx) error {
 	// var data map[string]string
 	var data models.User
-	
+	//parse the request body and decode it into the data variable
 	if err :=c.BodyParser(&data);err!= nil{
 		return err
 	}
@@ -21,18 +23,20 @@ func Register(c *fiber.Ctx) error {
 		Email:data.Email,
 		Password : password,
 	}
+	//This line uses the Create method of the database connection (database.DB) to insert the new user into the database
 	database.DB.Create(&user)
     return c.JSON(user)
 }
 func Login (c *fiber.Ctx)error{
 	// var data map[string]string
 	var data models.User
-	
+	//parse the request body and decode it into the data variable
 	if err :=c.BodyParser(&data);err!= nil{
 		return err
 	}
 	var user models.User
 	database.DB.Where("email = ?",data.Email).First(&user)
+	//If no user is found (checked by user.Id == 0)
 	if user.Id == 0{
 		c.Status(fiber.StatusNotFound)
 		return c.JSON(fiber.Map{
@@ -46,5 +50,26 @@ func Login (c *fiber.Ctx)error{
 		})
 	
 	}
-	return c.JSON(user)
+	//This function creates a new JWT token with the specified signing method
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer: strconv.Itoa(int(user.Id)),
+		ExpiresAt:time.Now().Add(time.Hour*24).Unix(),//1day
+	})
+	token,err:= claims.SignedString([]byte(SecretKey))
+	if err != nil{
+		c.Status(fiber.StatusInternalServerError)
+		return c.JSON(fiber.Map{
+			"message":"could not login",
+		})
+	}
+	cookie:= fiber.Cookie{
+		Name :"jwt",
+		Value : token,
+		Expires : time.Now().Add(time.Hour*24),
+		HTTPOnly: true,
+	}
+	c.Cookie(&cookie)
+	return c.JSON(fiber.Map{
+		"message":"success",
+	})
 }
--- main.go
+++ main.go
@@ -5,12 +5,17 @@ import (
 	"auth-go/database"
 	"auth-go/routes"
 	"github.com/gofiber/fiber/v2"
+    "github.com/gofiber/fiber/v2/middleware/cors"
 
 )
 
 func main() {
     database.Connect()
     app:=fiber.New()
+    
+    app.Use(cors.New(cors.Config{
+        AllowCredentials: true,
+    }))
     routes.Setup(app)
     //: This line instructs the web server to start listening on port 8000.
     app.Listen(":8000")
