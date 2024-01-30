6093af39dc1ca676d5506285a873882ea771f006
Dorra Nsir <dorransir24@gmail.com>
2024-01-30 11:37:18 +0100

    add login route and login function

--- controllers/authController.go
+++ controllers/authController.go
@@ -9,17 +9,42 @@ import (
 )
 
 func Register(c *fiber.Ctx) error {
-	var data map[string]string
+	// var data map[string]string
+	var data models.User
 	
 	if err :=c.BodyParser(&data);err!= nil{
 		return err
 	}
-	password,_ := bcrypt.GenerateFromPassword([]byte(data["password"]),14)
+	password,_ := bcrypt.GenerateFromPassword([]byte(data.Password),14)
 	user:=models.User{
-		Name:data["name"],
-		Email:data["email"],
+		Name:data.Name,
+		Email:data.Email,
 		Password : password,
 	}
 	database.DB.Create(&user)
     return c.JSON(user)
+}
+func Login (c *fiber.Ctx)error{
+	// var data map[string]string
+	var data models.User
+	
+	if err :=c.BodyParser(&data);err!= nil{
+		return err
+	}
+	var user models.User
+	database.DB.Where("email = ?",data.Email).First(&user)
+	if user.Id == 0{
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{
+			"message":"user not found",
+		})
+	}
+	if err:= bcrypt.CompareHashAndPassword(user.Password,[]byte(data.Password));err!=nil{
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message":"incorrect password",
+		})
+	
+	}
+	return c.JSON(user)
 }
